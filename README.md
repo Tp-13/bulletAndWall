b53918ca94cfbf66d661aecd877cf55f4e6df338
Tp-13 <66135230+Tp-13@users.noreply.github.com>
2020-06-16 11:57:32 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bulletAndWall
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,41 @@
+var wall,thickness;
+var bullet,speed,weight;
+var damage
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed = random(223,331);
+  weight = random(30,52)
+  thickness = random(22,83)
+
+  bullet = createSprite(50,200,75,40)
+  bullet.velocityX = speed;
+  bullet.shapeColor = "white";
+
+  wall = createSprite(1200,200,thickness,height/2)
+}
+
+function draw() {
+  background("black");
+
+  if (wall.x-bullet.x < bullet.width/2+wall.width/2){
+    damage = ((weight)*(speed)*(speed))/((thickness)*(thickness)*(thickness))
+    
+    if(damage < 10){
+      bullet.shapeColor = "green";
+    }
+    
+    if(damage > 10){
+      bullet.shapeColor = "red";
+    }
+    
+
+    bullet.velocityX = 0;
+    bullet.collide(wall);
+} 
+
+
+
+  drawSprites();
+}
